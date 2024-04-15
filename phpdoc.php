1af3054abc76e9872e47e974a306015c42474932
JuanCM <juancampuzano2356@gmail.com>
2024-04-15 17:28:42 -0500

    first commit

---
+++ phpdoc.php
@@ -0,0 +1,17 @@
+<?php 
+
+/**
+ * Esta función retorna la suma de dos números
+ *
+ * @param integer $n1
+ * @param integer $n2
+ * @return integer
+ */
+function suma(int $n1, int $n2){
+    return $n1 + $n2; 
+}
+
+$resultado = suma(1,2); 
+
+
+echo $resultado; 
